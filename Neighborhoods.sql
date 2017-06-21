e908ebbe12deca379f8ccd339beb8488cd9d515b
tyanay <yanaytomer@gmail.com>
2017-06-21 16:50:00 +0300

    SQL

---
+++ Neighborhoods.sql
@@ -0,0 +1,7 @@
+USE yelp_pittsburgh;
+
+select 	b.neighborhood as 'Neighborhood', count(r.review_id) as 'Number of reviews', avg(r.review_stars) as 'Avarage stars', count(b.business_id) as 'Number of Businesses in Neightborhood'
+from	fact_reviews r	left join dim_business b on b.business_id = r.business_id
+						#left join dim_users u on r.user_id = u.user_id
+						#left join dim_dates d on r.date_id = d.date_id
+group by b.neighborhood;
