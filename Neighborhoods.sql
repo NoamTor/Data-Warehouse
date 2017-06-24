24cc623464e7c78a9b16d59fa02340e6409feacd
yogevmatalon <yogevmatalon@gmail.com>
2017-06-24 17:50:09 +0300

    Update Neighborhoods.sql

--- Neighborhoods.sql
+++ Neighborhoods.sql
@@ -1,7 +1,9 @@
 USE yelp_pittsburgh;
 
-select 	b.neighborhood as 'Neighborhood', count(r.review_id) as 'Number of reviews', avg(r.review_stars) as 'Avarage stars', count(b.business_id) as 'Number of Businesses in Neightborhood'
+select 	b.neighborhood as 'Neighborhood', count(r.review_id) as 'Number of reviews', avg(r.review_stars) as 'Avarage stars', count(Distinct b.business_id) as 'Number of Businesses in Neightborhood'
 from	fact_reviews r	left join dim_business b on b.business_id = r.business_id
 						#left join dim_users u on r.user_id = u.user_id
 						#left join dim_dates d on r.date_id = d.date_id
-group by b.neighborhood;
+where neighborhood is not Null 
+group by b.neighborhood
+order by count(r.review_id) desc;
